fee397883967c74384ba1fa7076cef7ba9230a33
Aegsteh <1372118616@mail.dlut.edu.cn>
2023-03-14 20:42:34 +0800

    change FL.py

--- code/federated_learning.py
+++ code/federated_learning.py
@@ -6,21 +6,22 @@ import tools.utils
 from tools import jsonTool
 
 
-# load config
-config = jsonTool.generate_config('config.json')            # read config.json file and generate config dict
-client_config = config["client"]                    # get client's config
+if __name__ == "__main__":
+    # load config
+    config = jsonTool.generate_config('config.json')            # read config.json file and generate config dict
+    client_config = config["client"]                    # get client's config
 
-device = tools.utils.get_device(config["device"])               # get training device according to os platform, gpu or cpu
+    device = tools.utils.get_device(config["device"])               # get training device according to os platform, gpu or cpu
 
-mnist = MNIST()               # load whole dataset
-train_set = mnist.get_train_dataset()               # get global training set
-train_data,train_label = train_set.data,train_set.targets       # get training data and training label
-dataset0 = [train_data,train_label]                             # packege training data and training label
+    mnist = MNIST()               # load whole dataset
+    train_set = mnist.get_train_dataset()               # get global training set
+    train_data,train_label = train_set.data,train_set.targets       # get training data and training label
+    dataset0 = [train_data,train_label]                             # packege training data and training label
 
-test_set = mnist.get_test_dataset()                             # get testing data and testing label
+    test_set = mnist.get_test_dataset()                             # get testing data and testing label
 
-client0 = baseClient(cid=0,
-                     dataset=dataset0,
-                     client_config=client_config,
-                     device=device)
-client0.start()
+    client0 = baseClient(cid=0,
+                        dataset=dataset0,
+                        client_config=client_config,
+                        device=device)
+    client0.start()
